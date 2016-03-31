7933f377298ab1c7c973377ea8307d88931e7a2e
xharmagne <xharmagne@gmail.com>
2016-03-31 14:13:03 +1100

    Initial - Aurelia ES2016

---
+++ aurelia-es2016/build/paths.js
@@ -0,0 +1,19 @@
+var appRoot = 'src/';
+var outputRoot = 'dist/';
+var exportSrvRoot = 'export/';
+
+module.exports = {
+  root: appRoot,
+  source: appRoot + '**/*.js',
+  html: appRoot + '**/*.html',
+  css: appRoot + '**/*.css',
+  style: 'styles/**/*.css',
+  cssFramework: 'jspm_packages/github/zurb/foundation-sites*/dist/foundation.js',
+  sass: appRoot + 'scss/app.scss',
+  sassFramework: 'jspm_packages/github/zurb/foundation-sites@6.2.0/scss',
+  output: outputRoot,
+  exportSrv: exportSrvRoot,
+  doc: './doc',
+  e2eSpecsSrc: 'test/e2e/src/**/*.js',
+  e2eSpecsDist: 'test/e2e/dist/'
+};
